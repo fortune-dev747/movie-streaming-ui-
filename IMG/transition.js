08404691941d11fbf9f8c009a385d9e112f5cf85
Fortune Emmanuel <fortuneemmanuel18@gmail.com>
2026-05-27 06:19:12 +0100

    updated all

--- IMG/transition.js
+++ IMG/transition.js
@@ -1,1 +1,23 @@
 // Transition for the featured contents
+const first = document.getElementById("first");
+const nextTransition = document.getElementById("next-transition");
+const nextTransition2 = document.getElementById("twilight-transition");
+
+
+
+
+function minionTransition() {
+    first.style.display = "block";
+    nextTransition.style.display = "none";
+}
+function sonicTransition() {
+    first.style.display = "none";
+    nextTransition.style.display = "block";
+}
+function twilightTransition() {
+    nextTransition.style.display = "none";
+    nextTransition2.style.display = "block";
+}
+
+setInterval(minionTransition, 5000);
+setInterval(sonicTransition, 10000);
